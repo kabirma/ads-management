cdf143297cddcaa732515b9b675c153bc49afc7b
Kabir Khan <mak8523968@gmail.com>
2025-02-03 22:46:15 +0100

    Auth Work

--- resources/views/auth/setting.blade.php
+++ resources/views/auth/setting.blade.php
@@ -28,17 +28,30 @@
                     </div>
                     <div class="card-content">
                         <div class="card-body card-dashboard" style="padding-top: 0px;">
-                            <form action="{{ route('save.company') }}" method="post" class="row" enctype="multipart/form-data">
-                                @csrf
+                            @if (Session::has('success'))
+                                <div class="alert alert-success text-center">
+                                    <i class="fa fa-check"></i> {{ Session::get('success') }}
+                                </div>
+                            @endif
+
+                            @if (Session::has('error'))
+                                <div class="alert alert-danger text-center">
+                                    <i class="fa fa-times"></i> {{ Session::get('error') }}
+                                </div>
+                            @endif
                                 <input type="hidden" name="id" value="{{ isset($record) ? $record->id : 0 }}">
 
                                 <div class="col-md-12">
                                     <h4>Connect Social Media Account</h4>
                                     <hr>
                                 </div>
-
+                                
                                 <div class="form-group col-md-12">
-                                    <a href="{{route('redirect_to_tiktok')}}" class="btn btn-secondary"> <i class="fab fa-tiktok"></i> Conect TikTok</a>
+                                    @if(Auth::user()->tiktok_token !== null)
+                                        <a href="{{route('redirect_to_tiktok')}}" class="btn btn-secondary"> <i class="fab fa-tiktok"></i> Conect TikTok</a>
+                                    @else
+                                        <a href="#0" class="btn btn-success"> <i class="fab fa-tiktok"></i> TikTok Connected</a>
+                                    @endif
                                 </div>
                           
 
