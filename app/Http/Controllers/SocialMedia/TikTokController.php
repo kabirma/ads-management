82d6e21f72dcc24bd4f58d197177f244c7bc0d87
Kabir Khan <mak8523968@gmail.com>
2025-02-03 22:13:53 +0100

    Changes

--- app/Http/Controllers/SocialMedia/TikTokController.php
+++ app/Http/Controllers/SocialMedia/TikTokController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Auth;
+use App\Models\User;
 
 class TikTokController extends Controller
 {
